d4449e2cbff155295f2a0739109565a52dbc1106
Carlos Quintana Laynez <carlosquintanal.sanjosemlg@fundacionloyola.net>
2024-04-30 16:04:06 +0200

    Lee las etiquetas del json, falta leer el otro archivo

--- mainscript.js
+++ mainscript.js
@@ -15,13 +15,27 @@ class ScriptJson {
         // Pedir al usuario que ingrese la ruta del archivo
         this.rl.question('Inserte la ruta del archivo que desea abrir: ', (respuesta) => {
             // Abrir archivo ingresado por el usuario
-            fs.readFile(respuesta, 'utf8', (error, data) => {
-                if (error) {
-                    console.error(`Error al abrir el ${respuesta}:`, error);
+            try{
+                if (fs.existsSync(respuesta)) {
+                    // Verificar la extensión del archivo
+                    if (respuesta.endsWith('.js') || respuesta.endsWith('.html')) {
+                        // Leer el archivo
+                        fs.readFile(respuesta, 'utf8', (error, data) => {
+                            if (error) {
+                                console.error('Error al leer el archivo:', error);
+                            } else {
+                                console.log('Contenido del archivo:');
+                                console.log(data);
+                            }
+                        });
+                    } else {
+                        console.log('El archivo debe ser HTML o JavaScript (.js)');
+                        this.cerrarInterfaz();
+                    }
                 } else {
-                    console.log(`Archivo ${respuesta} encontrado`);
+                    console.log('El archivo no existe.');
+                    this.cerrarInterfaz();
                 }
-            });
             // Abrir archivo ejemploDicc.json
             fs.readFile(this.jsonDiccPath, 'utf8', (error, data) => {
                 if (error) {
@@ -32,13 +46,19 @@ class ScriptJson {
                         const jsonData = JSON.parse(data);
             
                         // Llamar a la función recursiva para leer todos los hijos del objeto JSON
-                        console.log('Contenido del archivo JSON:');
+                        //console.log('Contenido del archivo JSON:');
                         this.leerJson(jsonData);
+                        this.cerrarInterfaz();
                     } catch (error) {
                         console.error('Error al procesar el contenido JSON:', error);
                     }
                 }
             });
+            }catch{
+                console.log('No se encontró el archivo');
+            }finally{
+                this.cerrarInterfaz();
+            }
         });
     }
 
@@ -52,14 +72,14 @@ class ScriptJson {
                 const valor = objeto[clave];
     
                 // Imprimir la propiedad con indentación según el nivel de profundidad
-                console.log(`${' '.repeat(nivel * 2)}${clave}:`);
+                //console.log(`${' '.repeat(nivel * 2)}${clave}:`);
     
                 // Si el valor es un objeto (es decir, tiene propiedades), llamar recursivamente
                 if (typeof valor === 'object' && valor !== null) {
                     this.leerJson(valor, nivel + 1); // Llamada recursiva con nivel incrementado
                 } else {
                     // Si el valor no es un objeto, imprimir el valor
-                    console.log(`${' '.repeat((nivel + 1) * 2)}${valor}`);
+                    //console.log(clave);
                 }
             }
         }
